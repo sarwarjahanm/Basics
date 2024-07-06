b0c70caaf2cbd31f6426ad7cf166c93569d3b518
Sarwar Jahan M <sarwarjahan6628@gmail.com>
2024-07-06 13:48:46 +0530

    JS Demo

---
+++ JS/dbconnect.php
@@ -0,0 +1,12 @@
+<?php 
+
+	$servername = "localhost"; 
+	$username = "infoSecCamp"; 
+	$password = "infoSecCamp"; 
+
+	$database = "infoSecCamp"; 
+ 
+	$conn = mysqli_connect($servername, 
+		$username, $password, $database); 
+
+?> 
